a9940f611e0016573f7caac72b33d7ee1e9859bf
CAOYUE <prometheus.mr.cy@gmail.com>
2021-11-04 00:41:26 +0800

    两种模拟方式

--- simulation.py
+++ simulation.py
@@ -15,56 +15,40 @@ answer_list = []
 question_index = 0
 
 ok = "nok"
-users = {"曹越": "18811568652", "张浩": "13820896871", "魏明名": "18983097850",
-         "龚辉煌": "15300270049", "倪睿": "19801288725", "洪歌": "18811398104",
-         "孟祥梁": "13051328668", "孔海天": "15076000307", "贾璧莹": "15033598072",
-         "朱以坤": "15550781153", "李浩然": "13944676147"}
-'''
+
+options = webdriver.ChromeOptions()
+options.add_argument("user-agent=MicroMessenger")
+browser = webdriver.Chrome(options=options)
+browser.maximize_window()
+browser.get(url)
+
 while True:
     ok = input("登录后输入ok！")
     if ok == "ok":
         break
-'''
 
-for item in users.items():
-    options = webdriver.ChromeOptions()
-    options.add_argument("user-agent=MicroMessenger")
-    browser = webdriver.Chrome(options=options)
-    browser.maximize_window()
-    browser.get(url)
-    name = item[0]
-    passwd = item[1]
-    name_input = WebDriverWait(browser, 5, 0.5).until(EC.presence_of_element_located((By.CSS_SELECTOR, "body > div.login_ind > div.middle > div > div.inp > div:nth-child(1) > div.i > input")))
-    name_input.click()
-    name_input.send_keys(name)
-    passwd_input = browser.find_element_by_css_selector("body > div.login_ind > div.middle > div > div.inp > div:nth-child(2) > div.i > input")
-    passwd_input.click()
-    passwd_input.send_keys(passwd)
-    login = browser.find_element_by_css_selector("body > div.login_ind > div.middle > div > button")
-    login.click()
-    time.sleep(5)
-    for i in range(300):
-        browser.switch_to.window(browser.window_handles[0])
-        try:
-            start = WebDriverWait(browser, 2, 0.5).until(EC.presence_of_element_located((By.CLASS_NAME, "piecel-box")))
-            start.click()
-        except:
-            quit_play = WebDriverWait(browser, 5, 0.5).until(
-                EC.presence_of_element_located((By.CSS_SELECTOR, "body > div.button > ul > li:nth-child(2) > div")))
-            quit_play.click()
-        time.sleep(0.5)
-        ul = WebDriverWait(browser, 5, 0.1).until(EC.presence_of_element_located((By.ID, "o")))
-        li_list = ul.find_elements_by_tag_name("li")
-        li_right = li_list[0]
-        li_right.click()
-        time.sleep(0.2)
-        next_button = WebDriverWait(browser, 5, 0.5).until(EC.presence_of_element_located((By.CSS_SELECTOR, "body > div.button > ul > li:nth-child(2) > div")))
-        next_button.click()
-        time.sleep(1)
-        quit_play = WebDriverWait(browser, 5, 0.5).until(EC.presence_of_element_located((By.CSS_SELECTOR, "body > div.button > ul > li:nth-child(2) > div")))
+for i in range(300):
+    browser.switch_to.window(browser.window_handles[0])
+    try:
+        start = WebDriverWait(browser, 2, 0.5).until(EC.presence_of_element_located((By.CLASS_NAME, "piecel-box")))
+        start.click()
+    except:
+        quit_play = WebDriverWait(browser, 5, 0.5).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, "body > div.button > ul > li:nth-child(2) > div")))
         quit_play.click()
-        time.sleep(0.3)
-    browser.quit()
+    time.sleep(0.5)
+    ul = WebDriverWait(browser, 5, 0.1).until(EC.presence_of_element_located((By.ID, "o")))
+    li_list = ul.find_elements_by_tag_name("li")
+    li_right = li_list[0]
+    li_right.click()
+    time.sleep(0.2)
+    next_button = WebDriverWait(browser, 5, 0.5).until(EC.presence_of_element_located((By.CSS_SELECTOR, "body > div.button > ul > li:nth-child(2) > div")))
+    next_button.click()
+    time.sleep(1)
+    quit_play = WebDriverWait(browser, 5, 0.5).until(EC.presence_of_element_located((By.CSS_SELECTOR, "body > div.button > ul > li:nth-child(2) > div")))
+    quit_play.click()
+    time.sleep(0.3)
+browser.quit()
 
 
 
