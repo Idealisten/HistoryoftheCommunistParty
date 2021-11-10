0f7240e17c04a314fc87fc278422c42b34a7f3bf
CAOYUE <prometheus.mr.cy@gmail.com>
2021-11-10 21:30:33 +0800

    更新了单次循环答题

--- timing_answer.py
+++ timing_answer.py
@@ -13,6 +13,7 @@ answer_url = "http://ceshi.fotoncul.com.cn/Ajax/DataHandler.php"
 cookie = {}
 answer_list = []
 question_index = 0
+global li_right
 
 options = webdriver.ChromeOptions()
 options.add_argument("user-agent=MicroMessenger")
@@ -20,7 +21,8 @@ browser = webdriver.Chrome(options=options)
 browser.maximize_window()
 browser.get(url)
 wait_time = int(input("请输入答题时间："))
-ok = "nok"
+# wait_time = wait_time - 2
+# ok = "nok"
 
 
 def countdown():
@@ -28,63 +30,68 @@ def countdown():
     while wait_time > 0:
         # if ok == "ok":
         # print("\r" + str(wait_time), end="")
-        # print(str(wait_time))
         time.sleep(1)
         wait_time -= 1
         if wait_time == 0:
             break
 
 while True:
+    browser.get("http://ceshi.fotoncul.com.cn/title")
     start = WebDriverWait(browser, 5, 0.5).until(EC.presence_of_element_located((By.CLASS_NAME, "piecel-box")))
-    # start = WebDriverWait(browser, 5, 0.5).until(EC.presence_of_element_located(
-    # (By.XPATH, "/html/body/div[3]/div[1]/ul/li[2]/a/div")))
     t = threading.Thread(target=countdown)
     start.click()
     t.start()
-    while True:
-        ok = input("\n粘贴答案后输入ok！")
-        if ok == "ok":
-            break
     print("开始答题，请等待～")
-    ok = "no"
-    # ul = WebDriverWait(browser, 5, 0.5).until(EC.presence_of_element_located((By.ID, "o")))
-    # li_list = ul.find_elements_by_tag_name("li")
-    with open('/Users/CAOYUE/Desktop/题库1.txt') as f:
-        txt = f.read()
-        txt_list = demjson.decode(txt)
-        for question in txt_list:
-            if question["right"] == "A" or question["right"] == "a":
-                answer_list.append(0)
-            elif question["right"] == "B" or question["right"] == "b":
-                answer_list.append(1)
-            elif question["right"] == "C" or question["right"] == "c":
-                answer_list.append(2)
-            elif question["right"] == "D" or question["right"] == "d":
-                answer_list.append(3)
     while question_index < 99:
         # print("question_index:" + str(question_index))
-        # browser.switch_to.window(browser.window_handles[0])
-        # time.sleep(0.4)
+        if question_index == 0:
+            browser.switch_to.window(browser.window_handles[0])
+        time.sleep(0.2)
         ul = WebDriverWait(browser, 5, 0.1).until(EC.presence_of_element_located((By.ID, "o")))
+        time.sleep(0.2)
         li_list = ul.find_elements_by_tag_name("li")
+        time.sleep(0.2)
         p = browser.find_element_by_id("t")
         data_id = p.get_attribute("data-id")
-        if data_id == "691":
-            li_right = li_list[2]
-        elif data_id == "650":
+        # li_right = li_list[answer_list[question_index]]
+        question_right = p.get_attribute("da")
+        if question_right == "A" or question_right == "a":
+            li_right = li_list[0]
+            li_right.click()
+        elif question_right == "B" or question_right == "b":
             li_right = li_list[1]
-        else:
-            li_right = li_list[answer_list[question_index]]
-        li_right.click()
-        time.sleep(0.1)
+            li_right.click()
+        elif question_right == "C" or question_right == "c":
+            li_right = li_list[2]
+            li_right.click()
+        elif question_right == "D" or question_right == "d":
+            li_right = li_list[3]
+            li_right.click()
+        elif question_index == "650":
+            li_right = li_list[1]
+            li_right.click()
         question_index += 1
         next_button = browser.find_element_by_css_selector("body > div.button > ul > li:nth-child(2) > div")
         next_button.click()
-        time.sleep(1.6)
+        time.sleep(1.1)
     browser.switch_to.window(browser.window_handles[0])
     ul = WebDriverWait(browser, 5, 0.01).until(EC.presence_of_element_located((By.ID, "o")))
+    p = browser.find_element_by_id("t")
     li_list = ul.find_elements_by_tag_name("li")
-    li_right = li_list[answer_list[question_index]]
+    data_id = p.get_attribute("data-id")
+    question_right = p.get_attribute("da")
+    if question_right == "A" or question_right == "a":
+        li_right = li_list[0]
+        li_right.click()
+    elif question_right == "B" or question_right == "b":
+        li_right = li_list[1]
+        li_right.click()
+    elif question_right == "C" or question_right == "c":
+        li_right = li_list[2]
+        li_right.click()
+    elif question_right == "D" or question_right == "d":
+        li_right = li_list[3]
+        li_right.click()
     li_right.click()
     question_index += 1
     next_button = browser.find_element_by_css_selector("body > div.button > ul > li:nth-child(2) > div")
@@ -94,12 +101,13 @@ while True:
         # print("\n答题结束")
         break
     question_index = 0
-    answer_list.clear()
+
     again = input("\n答题结束，再答一次请返回首页后输入ok，退出输入quit")
     if again == "ok":
         wait_time = int(input("请输入答题时间："))
     else:
         break
+
 
 
 
